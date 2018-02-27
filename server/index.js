d1475e3ad193ec06f9e27f5bd1c2ad387f6c39a2
Pablo Prieto <pablotsdaw@gmail.com>
2018-02-27 12:19:00 +0100

    proyectoMEAN

--- server/index.js
+++ server/index.js
@@ -242,7 +242,7 @@ const passport = require('passport');
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get('/success', (req, res) => res.send("You have successfully logged in"));
+// app.get('/success', (req, res) => res.send("You have successfully logged in"));
 app.get('/error', (req, res) => res.send("error logging in"));
 
 passport.serializeUser(function(user, cb) {
